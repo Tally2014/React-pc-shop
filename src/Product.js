bf371ff3489b81923c60d7253b96766737a06856
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-12-12 23:10:19 +0200

    React onClick event

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 function Product(props) {
+
+    function purchase(item)
+    {
+        alert(`Puschasing this item ${item}`);
+    }
+
   return (
     <div className='product'>
         <img className='img' src={props.url}/>
         <h3>{props.name}</h3>
         <p>R{props.price}</p>
-        <button className='btn'>Purchase</button>
+        <button className='btn' onClick={()=>{
+            purchase(props.name)
+        }}>Purchase</button>
     </div>
   )
 }
